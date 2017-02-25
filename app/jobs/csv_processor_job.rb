0fa15e801924da021d8c0a9f68cd58bb286f16b0
Gaston Trujillo <gaston.trujillo.java@gmail.com>
2017-02-24 22:04:32 -0500

    Reenqueue if Term signal

--- app/jobs/csv_processor_job.rb
+++ app/jobs/csv_processor_job.rb
@@ -1,5 +1,7 @@
 class CsvProcessorJob < ActiveJob::Base
   def perform(processor)
     processor.process_csv
+  rescue Resque::TermException
+    Resque.enqueue(CsvProcessorJob, processor)
   end
 end
